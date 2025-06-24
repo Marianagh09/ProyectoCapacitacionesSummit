de1dd56aac94076003158b1ff352ce8e6400e791
Mariana Garcia Hincapie <mgarciah@SUMMIT.COM.CO>
2025-06-24 16:44:07 -0500

    agregue el PostAssignedCourseData

--- CAP.Courses/PostAssignedCourseData.cs
+++ CAP.Courses/PostAssignedCourseData.cs
@@ -5,6 +5,10 @@ using System.Text;
 using System.Threading.Tasks;
 using Sif;
 using Sif.Data;
+using Sif.ImEx;
+using Sif.Journal;
+using Sif.Security;
+using Sif.Security.Roles;
 using Sif.Services;
 
 namespace CAP.Courses
@@ -20,8 +24,20 @@ namespace CAP.Courses
 			ServiceState state = ServiceState.Rejected;
 			using (SifDBCommand command = DBFactory.DefaultFactory.NewDBCommand(fCurses, this.Connection))
 			{
-				  
+				  command.AddParameter(this.Dictionary.ImEx, DataDictImEx.JoinIdName, this.Dictionary.ImEx.JoinId);
+				  command.AddParameter(this.Dictionary.Journal, DataDictJournal.LineStatusName, this.Dictionary.Journal.LineStatus);
+				  command.AddParameter(this.Dictionary.Journal, DataDictJournal.ServiceStartDateTimeName, this.Dictionary.Journal.LineStatus);
+				  command.AddParameter(this.Dictionary.Roles, DataDictRoles.DestinationRoleIdName, this.Dictionary.Roles.DestinationRoleId);
+				  command.AddParameter(this.Dictionary.Security, DataDictSecurity.TellerIdName, this.Dictionary.Security.TellerId);
+				Int32 rows = command.ExecuteNonQuery(this.Message);
+				if (rows > 0)
+				{
+					state = ServiceState.Accepted;
+				}
+				return state;
 			}
 		}
+
+		private static readonly String fCurses = ""; 
 	}
 }
